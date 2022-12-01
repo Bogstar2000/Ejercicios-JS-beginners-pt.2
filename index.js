bb3ef2cffbb698057321e8ae12c9eb0aad2acdaa
Bogstar2000 <98562378+Bogstar2000@users.noreply.github.com>
2022-11-30 19:06:06 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var num10 = prompt('escribe el num1');
+var num12 = prompt('escribe el num2');
+
+if (num10 => num12) {
+  console.log('num1 es mayor a num2');
+} else {
+  console.log('num2 es nayor a num1');
+}
+ 
